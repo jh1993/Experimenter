35755dda2e33ad2644524c1c2ec29be3fd97d460
JohnSolaris <10736610+jh1993@users.noreply.github.com>
2022-09-12 21:39:24 -0700

    aaaaaaaaa

---
+++ Experimenter.py
@@ -0,0 +1,93 @@
+import Mutators, Shrines, Level, Consumables
+import mods.API_Universal.APIs.API_Spells.API_Spells as SpellAPI
+
+class Experimenter1(Mutators.Mutator):
+
+    def __init__(self):
+        Mutators.Mutator.__init__(self)
+        self.description = ("Realms 1 and 2 contain no enemies or gates\n"
+                            "Realm 2 always contains a circle\n"
+                            "Realm 3 always contains a shrine")
+        self.all_valid = SpellAPI.primary_shrine_option + SpellAPI.secondary_shrine_option + SpellAPI.tertiary_shrine_option
+    
+    def on_levelgen_pre(self, levelgen):
+        if levelgen.difficulty <= 2:
+            levelgen.num_generators = 0
+            levelgen.num_monsters = 0
+            levelgen.bosses = []
+        if levelgen.game and levelgen.difficulty == 2:
+            levelgen.shrine = Shrines.library(levelgen.game.p1)
+        if levelgen.game and levelgen.difficulty == 3:
+            levelgen.shrine = Shrines.shrine(levelgen.game.p1)
+
+class Experimenter2(Mutators.Mutator):
+
+    def __init__(self):
+        Mutators.Mutator.__init__(self)
+        self.description = ("Realms 1 and 2 contain no enemies or gates\n"
+                            "Realm 1 contains one rift for each circle\n"
+                            "Realm 2 contains one rift for each shrine\n")
+        self.all_valid = SpellAPI.primary_shrine_option + SpellAPI.secondary_shrine_option + SpellAPI.tertiary_shrine_option
+    
+    def on_levelgen_pre(self, levelgen):
+        if levelgen.difficulty <= 2:
+            levelgen.num_generators = 0
+            levelgen.num_monsters = 0
+            levelgen.bosses = []
+            levelgen.num_exits = len(self.all_valid) if levelgen.difficulty != 2 else len(Shrines.new_shrines)
+        if levelgen.game and levelgen.difficulty == 2:
+            levelgen.shrine = Shrines.library(levelgen.game.p1)
+        if levelgen.game and levelgen.difficulty == 3:
+            levelgen.shrine = Shrines.shrine(levelgen.game.p1)
+    
+    def on_levelgen(self, levelgen):
+        if levelgen.difficulty == 2:
+            all_port_tiles = [t for t in levelgen.level.iter_tiles() if type(t.prop) == Level.Portal]
+            for tile, shrine in zip(all_port_tiles, Shrines.new_shrines):
+                tile.prop.level_gen_params.shrine = Shrines.make_shrine(shrine[0](), levelgen.game.p1)
+        if levelgen.difficulty == 1:
+            all_port_tiles = [t for t in levelgen.level.iter_tiles() if type(t.prop) == Level.Portal]
+            for tile, tag in zip(all_port_tiles, self.all_valid):
+                tile.prop.level_gen_params.shrine = Level.PlaceOfPower(tag)
+
+class Experimenter3(Mutators.Mutator):
+
+    def __init__(self):
+        Mutators.Mutator.__init__(self)
+        self.description = ("You start with 10 SP and an extra Dragon Horn\n"
+                            "Realms 1 and 2 contain no enemies, gates, items, or SP\n"
+                            "Realm 1 contains one rift for each circle and realm 2 contains one rift for each shrine")
+        self.all_valid = SpellAPI.primary_shrine_option + SpellAPI.secondary_shrine_option + SpellAPI.tertiary_shrine_option
+    
+    def on_levelgen_pre(self, levelgen):
+        if levelgen.difficulty <= 2:
+            levelgen.num_generators = 0
+            levelgen.num_monsters = 0
+            levelgen.num_recharges = 0
+            levelgen.num_heals = 0
+            levelgen.num_xp = 0
+            levelgen.bosses = []
+            levelgen.items = []
+            levelgen.num_exits = len(self.all_valid) if levelgen.difficulty != 2 else len(Shrines.new_shrines)
+        if levelgen.game and levelgen.difficulty == 2:
+            levelgen.shrine = Shrines.library(levelgen.game.p1)
+        if levelgen.game and levelgen.difficulty == 3:
+            levelgen.shrine = Shrines.shrine(levelgen.game.p1)
+    
+    def on_levelgen(self, levelgen):
+        if levelgen.difficulty == 2:
+            all_port_tiles = [t for t in levelgen.level.iter_tiles() if type(t.prop) == Level.Portal]
+            for tile, shrine in zip(all_port_tiles, Shrines.new_shrines):
+                tile.prop.level_gen_params.shrine = Shrines.make_shrine(shrine[0](), levelgen.game.p1)
+        if levelgen.difficulty == 1:
+            all_port_tiles = [t for t in levelgen.level.iter_tiles() if type(t.prop) == Level.Portal]
+            for tile, tag in zip(all_port_tiles, self.all_valid):
+                tile.prop.level_gen_params.shrine = Level.PlaceOfPower(tag)
+            
+    def on_game_begin(self, game):
+        game.p1.xp = 10
+        game.p1.add_item(Consumables.dragon_horn())
+
+Mutators.all_trials.append(Mutators.Trial("Experimenter", Experimenter1()))
+Mutators.all_trials.append(Mutators.Trial("Experimenter Ultimate", Experimenter2()))
+Mutators.all_trials.append(Mutators.Trial("Experimenter Unhinged", Experimenter3()))
---
+++ README.txt
@@ -0,0 +1,13 @@
+This mod requires API_Universal, which can be found here: https://discord.com/channels/645130221435092992/994785352793473104
+
+To install this mod, please rename the "Experimenter-main" folder to "Experimenter" before putting it into your mods folder.
+
+This mod adds a new gameplay mode, Experimenter, implemented as archmage trials. It is meant to allow you to experiment with certain spell and shrine combinations without having to struggle with the hardest parts of very early game. You will be able to, for instance, make the very questionable decision of starting with a level 7 spell and no cantrips, and similar shenanigans.
+
+There are three variants of Experimenter, with increasing levels of degeneracy.
+
+1. Experimenter: The most basic version. The first two realms contain no enemies, allowing you to forego having to take a cantrip or anything else for very early game survival. Realm 2 always contains a circle, allowing you to use up to 7 SP to buy a single tag of spells and skills at a discount. Realm 3 always contains a shrine, allowing you to experiment with certain spell and shrine combinations that you otherwise may not have the chance to experience. However, it's still up to RNG what circles and shrines you get.
+
+2. Experimenter Ultimate: The much more degenerate version, which eliminates RNG almost entirely by making sure realm 1 contains a rift for every circle, and realm 2 contains a rift for every shrine. This is like level 1, except you can choose exactly which circle and shrine you want. The only RNG involves the items that you get on realm 2.
+
+3. Experimenter Unhinged: The version with close to maximum possible degeneracy. Realms 1 and 2 contain no items or SP, but you instead start with 10 SP. This simulates the possibility that you're lucky enough to find a Draught of Memories on realm 2, and were able to grab all 3 SP orbs on the level within the duration without needing to use your starting Teleporter. On top of that, you also start with an extra Dragon Horn, as if you also found it on realm 2, allowing you to skip any of the next few realms at your leisure. Aside from eliminating the need to start with a cantrip to clear realm 1, this is still theoretically within the realm of possibility for the exorbitantly lucky, which should hopefully alleviate some of the guilt that you may be considered blatantly cheating by playing on this mode.
